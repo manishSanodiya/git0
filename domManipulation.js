7a226baa8f5df08f5cbbe8d9e4a9aded43c166fd
manishSanodiya <122765253+manishSanodiya@users.noreply.github.com>
2023-01-16 18:07:12 +0530

    Add files via upload

--- domManipulation.js
+++ domManipulation.js
@@ -9,7 +9,18 @@ let header = document.getElementById('main-header');
 // headerTitle.innerHTML = '<h3>Hello</h3'
 header.style.borderBottom = 'solid 4px black';
 let t = document.getElementsByClassName('title');
-console.log(t);
+// console.log(t);
 let a = document.querySelector('.title');
-console.log(a);
+// console.log(a);
 a.style.color = 'green';
+let li = document.getElementsByClassName("list-group-item");
+li[2].style.backgroundColor="green";
+li[0].style.fontWeight = 'bold';
+li[1].style.fontWeight = 'bold';
+li[2].style.fontWeight = 'bold';
+li[3].style.fontWeight = 'bold';
+
+let li2 = document.getElementsByTagName('li');
+for(let i=0; i<li2.length; i++){
+    li2[i].style.backgroundColor="grey";
+}
